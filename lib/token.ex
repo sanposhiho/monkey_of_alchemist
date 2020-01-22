6895fe143e9f3dc5952628c6b95018677d94d422
sanposhiho <handbomusic@gmail.com>
2020-01-22 15:39:23 +0900

    make test

--- lib/token.ex
+++ lib/token.ex
@@ -1,4 +1,31 @@
 defmodule Token do
   defstruct [:type, :literal]
 
+  defmacro tokentype do
+    %{
+      illegal: "illegal",
+      eof: "eof",
+
+      #識別子 + リテラル
+      ident: "ident",
+      int: "int",
+
+      #演算子
+      assign: "=",
+      plus: "+",
+
+      #デリミタ
+      comma: ",",
+      semicolon: ";",
+
+      lparen: "(",
+      rparen: ")",
+      lbrace: "{",
+      rbrace: "}",
+
+      #キーワード
+      function: "function",
+      let: "let",
+    }
+  end
 end
